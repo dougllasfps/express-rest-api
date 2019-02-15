a7c07910f133dad8dc5f4fd7cce16119c70f25dc
Dougllas Sousa <dougllasfps@gmail.com>
2019-02-15 16:52:10 -0300

    Adicionando consign

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,4 +1,9 @@
 const httpUtil = require('../util/httpUtil')
+const { check, validationResult } = require('express-validator/check');
+
+const validation = [
+    check('email').isEmail()
+]
 
 const post = ( req, res ) => {
     res.status(201).send(req.body)
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,4 +1,4 @@
 module.exports = app => {
     app.route('/api/users')
-        .post(app.controller.userController.post)
+        .post(app.controller.userController.post, app.controller.userController.validation)
 }
